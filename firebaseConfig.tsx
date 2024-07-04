52d09d9e68bef710466ca14b415adab013855c64
Eren AVAR <eren.avar@hotmail.com>
2024-07-04 18:07:28 +0300

    install firebase and create firebaseconfig.tsx

---
+++ firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD8CaNG5Q-lJRIXPFjubYXaYfSD4IHxf8Y",
+    authDomain: "login-page-with-firebase-aa24a.firebaseapp.com",
+    projectId: "login-page-with-firebase-aa24a",
+    storageBucket: "login-page-with-firebase-aa24a.appspot.com",
+    messagingSenderId: "1068062763791",
+    appId: "1:1068062763791:web:e4b75e88349a8c8de96c7a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
