1704f8172cad76fdcecf9342e131dc279308bc28
Eren AVAR <eren.avar@hotmail.com>
2024-07-07 12:21:18 +0300

    add splashscreen codes and change splash image

--- src/navigation/mainNavigation.tsx
+++ src/navigation/mainNavigation.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { RootStackParamList } from './types';
+import SplashScreen from '../screens/SplashScreen';
 
 
 
@@ -13,6 +14,7 @@ function MainNavigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Profile" component={ProfileScreen} />
             </Stack.Navigator>
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { FC, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../../firebaseConfig'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigation/types'
 
-const SplashScreen = () => {
-  return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
+type Props = NativeStackScreenProps<RootStackParamList, "Splash">
+
+const SplashScreen: FC<Props> = ({ navigation }) => {
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user?.uid) {
+                navigation.navigate("Profile")
+            }
+        })
+    }, [])
+    return (
+        <View style={styles.container}>
+            <Image source={require("../../assets/icon.png")} style={styles.image} />
+        </View>
+    )
 }
 
 export default SplashScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white"
+    },
+    image: {
+        height: 200,
+        width: 200
+    }
+
+})
