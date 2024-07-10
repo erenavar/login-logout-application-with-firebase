5ab371dc58ac0246dc92daace16dd8f7f928e1f6
Eren AVAR <eren.avar@hotmail.com>
2024-07-10 04:33:32 +0300

    add inputs for update and css

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { FC } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import Button from '../components/Button';
-import { signOut } from 'firebase/auth';
+import { getAuth, signOut } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
+import Input from '../components/Input';
+
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
@@ -12,9 +14,15 @@ const ProfileScreen: FC<Props> = () => {
   const logOut = () => {
     signOut(auth)
   }
+  const auth = getAuth();
+
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Input value={auth.currentUser.email || ""} />
+        <Input value={auth.currentUser.displayName || ""} />
+        <Input value={auth.currentUser.photoURL || ""} />
+      </ScrollView>
       <Button title='Log Out' onPress={logOut} backgroundColor="red" />
     </View>
   )
@@ -26,6 +34,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-
+  },
+  scrollView: {
+    flexGrow: 1,
   }
 })
