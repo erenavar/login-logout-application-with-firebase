354bb0d2dda622503a3b0b7f09c8047c1dec13a5
Eren AVAR <eren.avar@hotmail.com>
2024-07-12 09:19:58 +0300

    add update feature

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,9 +1,9 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { FC } from 'react'
+import React, { FC, useReducer, useRef, useState } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import Button from '../components/Button';
-import { getAuth, signOut } from 'firebase/auth';
+import { getAuth, signOut, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 import Input from '../components/Input';
 
@@ -15,14 +15,40 @@ const ProfileScreen: FC<Props> = () => {
     signOut(auth)
   }
   const auth = getAuth();
+  const [displayName, setDisplayName] = useState(auth.currentUser?.displayName || "");
+  const [photoURL, setphotoURL] = useState(auth.currentUser?.photoURL || "")
 
+
+
+  const update = async () => {
+
+    if (auth.currentUser) {
+      try {
+        await updateProfile(auth.currentUser, {
+          displayName,
+          photoURL
+        })
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+
+    }
+  }
+  const changeDisplayName = (value: string) => {
+    setDisplayName(value)
+  }
+
+  const changePhotoURL = (value: string) => {
+    setphotoURL(value)
+  }
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <Input value={auth.currentUser.email || ""} />
-        <Input value={auth.currentUser.displayName || ""} />
-        <Input value={auth.currentUser.photoURL || ""} />
+        <Input value={displayName} onChangeText={text => setDisplayName(text)} />
+        <Input value={photoURL} onChangeText={text => setphotoURL(text)} autoCapitalize='none' />
       </ScrollView>
+      <Button title='Update' onPress={update} />
       <Button title='Log Out' onPress={logOut} backgroundColor="red" />
     </View>
   )
