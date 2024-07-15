fdb4a642c4d7524983b5fc9e10ea7869f77be4f8
Eren AVAR <eren.avar@hotmail.com>
2024-07-15 17:50:49 +0300

    add spinner to add product page

--- src/navigation/mainNavigation.tsx
+++ src/navigation/mainNavigation.tsx
@@ -15,6 +15,7 @@ function MainNavigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen name="Add" component={AddProductScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
                 <Stack.Screen name='AddProduct' component={AddProductScreen} options={{ headerShown: false }} />
--- src/screens/AddProductScreen.tsx
+++ src/screens/AddProductScreen.tsx
@@ -4,25 +4,31 @@ import Input from '../components/Input'
 import Button from '../components/Button'
 import { addDoc, collection } from 'firebase/firestore'
 import { db } from '../../firebaseConfig'
+import Spinner from '../components/Spinner'
 
 const AddProductScreen = () => {
     const [state, setState] = useState({
         title: "",
         description: "",
         image: "",
-        price: ""
+        price: "",
+        loader: false
     })
     const save = async () => {
 
         try {
-            const docRef = await addDoc(collection(db, "users"), {
+            setState((prevState) => ({ ...prevState, loader: true }))
+            const docRef = await addDoc(collection(db, "products"), {
                 title: state.title,
                 description: state.description,
                 image: state.image,
                 price: parseInt(state.price)
             })
-
-        } catch (error) {
+            console.log('Document written with ID: ', docRef.id);
+        } catch (e) {
+            console.log('Error adding document: ', e)
+        } finally {
+            setState((prevState) => ({ ...prevState, loader: false }))
 
         }
 
@@ -33,9 +39,10 @@ const AddProductScreen = () => {
                 <Input value={state.title} placeholder='Title' onChangeText={(text) => setState((prevState) => ({ ...prevState, title: text }))} keyboardType="default" />
                 <Input value={state.description} placeholder='Description' onChangeText={(text) => setState((prevState) => ({ ...prevState, description: text }))} />
                 <Input value={state.image} placeholder='Image' onChangeText={(text) => setState((prevState) => ({ ...prevState, image: text }))} />
-                <Input value={state.price} placeholder='Price' onChangeText={(text) => setState((prevState) => ({ ...prevState, price: text }))} keyboardType="numeric" />
+                <Input value={state.price} placeholder='Price' onChangeText={(text) => setState((prevState) => ({ ...prevState, price: text }))} keyboardType="number-pad" />
                 <Button onPress={save} title='Save'></Button>
             </View>
+            <Spinner visible={state.loader} />
         </SafeAreaView>
     )
 }
