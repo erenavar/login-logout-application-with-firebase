4290d0e82be13b74d5f31ef679385f48d456e3aa
Eren AVAR <eren.avar@hotmail.com>
2024-07-16 23:47:23 +0300

    complete product card

--- src/screens/Product/productListItem.tsx
+++ src/screens/Product/productListItem.tsx
@@ -1,15 +1,27 @@
-import { Image, Text, View } from "react-native"
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native"
 import { IProduct } from "./types"
 
 
-
+const { width } = Dimensions.get("window");
 const renderProduct = (item: IProduct) => {
     return (
-        <View>
-            <Image source={{ uri: item.imageURL }} style={{ height: 100, width: 100 }} />
+        <View style={styles.container}>
+            <Image source={{ uri: item.imageURL }} style={styles.image} />
             <Text>{item.title}</Text>
+            <Text>${item.price}</Text>
         </View>
     )
 }
 
 export { renderProduct };
+const itemWidth = width / 2 - 16;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 8,
+        width: itemWidth,
+        maxWidth: itemWidth,
+    },
+    image: { height: itemWidth, width: itemWidth }
+})
